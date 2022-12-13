aa7f6719c5f657ed5921257b1a600a3871f572b5
KrystianMaciejewski <89975505+KrystianMaciejewski@users.noreply.github.com>
2022-12-13 21:39:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let homeScoreEl = document.getElementById("homescore-el")
+let homeScore = 0;
+homeScoreEl.innerText = homeScore;
+let guestScoreEl = document.getElementById("questscore-el")
+let guestScore = 0;
+guestScoreEl.innerText = guestScore;
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+
+function incrementHome(point) {
+    homeScore += point;
+    homeScoreEl.innerText = homeScore;
+    checkwinner();
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+function incrementGuest(point) {
+    guestScore += point;
+    guestScoreEl.innerText = guestScore;
+    checkwinner();
+      
+}
+
+function refresh() {
+    homeScore = 0;
+    homeScoreEl.innerText = homeScore;
+    guestScore = 0;
+    guestScoreEl.innerText = guestScore;
+    homeScoreEl.style.textDecoration = "none";
+    guestScoreEl.style.textDecoration = "none";
+}
+
+function checkwinner(){
+    homeScoreEl.style.textDecoration = "none";
+    guestScoreEl.style.textDecoration = "none";
+    if (homeScore < guestScore) {
+        guestScoreEl.style.textDecoration = "underline";
+    }
+    if (homeScore > guestScore) {
+        homeScoreEl.style.textDecoration = "underline";
+    }
+}
+function getRandomInt(max) {
+  let random = Math.floor(Math.random() * max);
+    homeScore = random;
+    homeScoreEl.innerText = homeScore;
+    random = Math.floor(Math.random() * max);
+    guestScore = random;
+    guestScoreEl.innerText = guestScore;
+    checkwinner();
 }
